3df8ac14dd3ef09badfa466acf68aea6bbccd058
Vojtech Juranek <vjuranek@redhat.com>
2013-07-16 13:55:05 +0200

    BeakerBuilder descriptor

--- src/main/java/org/jenkinsci/plugins/beakerbuilder/BeakerBuilder.java
+++ src/main/java/org/jenkinsci/plugins/beakerbuilder/BeakerBuilder.java
@@ -7,16 +7,9 @@ import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
-import hudson.util.FormValidation;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-
 import net.sf.json.JSONObject;
 
 import org.kohsuke.stapler.DataBoundConstructor;
-import org.kohsuke.stapler.QueryParameter;
 import org.kohsuke.stapler.StaplerRequest;
 
 public class BeakerBuilder extends Builder {
@@ -36,40 +29,18 @@ public class BeakerBuilder extends Builder {
         return (DescriptorImpl)super.getDescriptor();
     }
 
-    /**
-     * Descriptor for {@link HelloWorldBuilder}. Used as a singleton.
-     * The class is marked as public so that it can be accessed from views.
-     *
-     * <p>
-     * See <tt>src/main/resources/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
-     * for the actual HTML fragment for the configuration screen.
-     */
-    @Extension // This indicates to Jenkins that this is an implementation of an extension point.
+    @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
         
         private String beakerURL;
         private String login;
         private String password;
-
-        /**
-         * Performs on-the-fly validation of the form field 'name'.
-         *
-         * @param value
-         *      This parameter receives the value that the user has typed.
-         * @return
-         *      Indicates the outcome of the validation. This is sent to the browser.
-         */
-        public FormValidation doCheckName(@QueryParameter String value)
-                throws IOException, ServletException {
-            if (value.length() == 0)
-                return FormValidation.error("Please set a name");
-            if (value.length() < 4)
-                return FormValidation.warning("Isn't the name too short?");
-            return FormValidation.ok();
+        
+        public DescriptorImpl() {
+            load();
         }
 
-        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
-            // Indicates that this builder can be used with all kinds of project types 
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) { 
             return true;
         }
 
@@ -80,7 +51,6 @@ public class BeakerBuilder extends Builder {
         @Override
         public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
             req.bindJSON(this, formData);
-            //beakerURL = formData.getString("beakerURL");
             save();
             return super.configure(req,formData);
         }
@@ -88,14 +58,28 @@ public class BeakerBuilder extends Builder {
         public String getBeakerURL() {
             return beakerURL;
         }
-        
+
+        public void setBeakerURL(String beakerURL) {
+            this.beakerURL = beakerURL;
+        }
+
         public String getLogin() {
             return login;
         }
-        
+
+        public void setLogin(String login) {
+            this.login = login;
+        }
+
         public String getPassword() {
             return password;
         }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        
     }
 
 }
