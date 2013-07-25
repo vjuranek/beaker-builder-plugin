79690f3e39223f9ce876521b7a981d2bc2057bd2
Vojtech Juranek <vjuranek@redhat.com>
2013-07-25 13:15:51 +0200

    Add exception logging and javadoc

--- src/main/java/org/jenkinsci/plugins/beakerbuilder/BeakerEnvContributor.java
+++ src/main/java/org/jenkinsci/plugins/beakerbuilder/BeakerEnvContributor.java
@@ -1,6 +1,7 @@
 package org.jenkinsci.plugins.beakerbuilder;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import hudson.EnvVars;
 import hudson.Extension;
@@ -8,18 +9,27 @@ import hudson.model.EnvironmentContributor;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 
+/**
+ * Exports Beaker related stuff like job ID in build env. variables for further use of subsequent steps.
+ * 
+ * @author vjuranek
+ * 
+ */
+
 @Extension
 public class BeakerEnvContributor extends EnvironmentContributor {
 
     @Override
-    public void buildEnvironmentFor(@SuppressWarnings("rawtypes") Run r, EnvVars envs, TaskListener listener) throws IOException, InterruptedException {
+    public void buildEnvironmentFor(@SuppressWarnings("rawtypes") Run r, EnvVars envs, TaskListener listener)
+            throws IOException, InterruptedException {
         BeakerBuildAction bba = r.getAction(BeakerBuildAction.class);
-        if(bba != null)
+        if (bba != null)
             try {
                 envs.put("BEAKER_JOB_ID", String.valueOf(bba.getJobNumber()));
-            } catch(NumberFormatException e) {
-                // TODO log the exception
+            } catch (NumberFormatException e) {
+                LOGGER.warning("Cannot convert " + bba.getJobNumber() + " to integer");
             }
     }
-
+    
+    private static final Logger LOGGER = Logger.getLogger(BeakerEnvContributor.class.getName());
 }
